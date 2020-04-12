2d0aad14fef1c493deef061386d3459aad1078f4
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-04-13 05:05:17 +0800

    adding a lot of files

---
+++ MyFirstConsoleApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+         {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from webgentle application");
+                });
+            });
+        }
+    }
+}
